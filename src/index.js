cc9ce75674b031d5736fc3c419adc06407c3bd92
TechBree <bridget_kodohu@yahoo.com>
2025-01-18 13:50:14 +0100

    create the basic JS for the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function createFact(event) {
+  event.preventDefault();
 
+  new Typewriter("#food", {
+    strings: ["Pizza is orginally italian"],
+    autoStart: true,
+    delay: 20,
+  });
+}
+
+let foodFactGenerator = document.querySelector("#food-generator");
+foodFactGenerator.addEventListener("submit", createFact);
